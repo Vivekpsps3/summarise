8e7277c455121b7ed96f67e805f45ddd83b0a685
vivekpsps3 <vivek.panchagnula@gmail.com>
2024-01-11 17:05:29 -0500

    init

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { DashComponent } from '../dash/dash.component';
+import path from 'path';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { component: DashComponent, path: 'dash' }
+];
